d8d6ace1ab08de369c1f7be927964873f011b532
jlpouffier <jean.loic.pouffier@gmail.com>
2021-05-20 18:20:18 +0200

    Google Home is now stopped when I leave (Security)

--- appdaemon/apps/monitor_home.py
+++ appdaemon/apps/monitor_home.py
@@ -1,7 +1,10 @@
 import hassapi as hass
 
 """
-monitor_home is an app responsible of the monitoring the home and sending notifications
+monitor_home is an app responsible of the monitoring the home 
+
+Goals :
+. Turn on and off Google Home based on home occupancy
 
 Notifications :
 . Home empty and Lights on 
@@ -15,6 +18,7 @@ Notifications :
 class monitor_home(hass.Hass): 
   def initialize(self):
     self.listen_state(self.callback_home_empty , "binary_sensor.home_occupied" , old = "on" , new = "off")
+    self.listen_state(self.callback_home_occupied , "binary_sensor.home_occupied" , old = "off" , new = "on")
     self.listen_state(self.callback_coffee_maker_on_since_too_long , "switch.coffee_maker" , new = "on", duration = 5400)
 
     self.log("Monitor Home initialized")
@@ -22,9 +26,14 @@ class monitor_home(hass.Hass):
   """
   Callback triggered when the home becomes not occupied
   Goals :
+  . Turn off Google home
   . Send notification(s)
   """
   def callback_home_empty(self, entity, attribute, old, new, kwargs):
+    # home empty >> stopping google home
+    self.log("Detecting home empty... Stopping Google Home")
+    self.call_service("switch/turn_off" , entity_id = "switch.google_home")
+
     # test if lights are still on
     if self.get_state("light.interior_lights") == "on":
       self.log("Detecting home empty and lights on. Notifying it...")
@@ -47,10 +56,21 @@ class monitor_home(hass.Hass):
 
 
   """
+  Callback triggered when the home becomes occupied
+  Goals :
+  . Turn on Google home
+  """
+  def callback_home_occupied(self, entity, attribute, old, new, kwargs):
+    # home occupied >> starting google home
+    self.log("Detecting home occupied... Starting Google Home")
+    self.call_service("switch/turn_on" , entity_id = "switch.google_home")
+
+
+  """
   Callback triggered when coffee maker on for more than 90 minutes
   Goals :
   . Send notification(s)
   """
-  def callback_coffee_maker_still_on(self, entity, attribute, old, new, kwargs):
+  def callback_coffee_maker_on_since_too_long(self, entity, attribute, old, new, kwargs):
     self.log("Detecting coffee maker on for more than 90 minutes. Notifying it...")
     self.fire_event("NOTIFY", payload = "coffee_maker_on_since_too_long")
