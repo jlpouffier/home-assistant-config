c9c971946254fc0b480b3b39aa25e4040ac8e2cb
jlpouffier <jean.loic.pouffier@gmail.com>
2021-11-09 23:15:36 +0100

    watch_tv refactor to handle a nasty edge case.

--- appdaemon/apps/watch_tv.py
+++ appdaemon/apps/watch_tv.py
@@ -9,7 +9,7 @@ Functionalities :
 
 List of apps driving Lights:
 . Netflix
-. Kodi
+. Plex
 . Amazon Prime
 
 """
@@ -37,22 +37,22 @@ class watch_tv(hass.Hass):
       current_app = "com.google.android.tvlauncher"
     
     #LOG
-    self.log("[" + self.old_app + " > " + current_app + "] : " + old + " > " + new)
+    self.log("State changed: " + old + " @ " + self.translate_app(self.old_app) + " > " + new + " @ " + self.translate_app(current_app))
     
     if old in ["off", "standby", "unavailable" , "paused", "unknown", "idle"] and new == "playing":
-      if self.is_controling_lights(self.old_app, current_app):
+      if self.is_app_controling_lights(current_app):
         #CALL SCRIPT
         self.log("TV playing : Lights dimmed")
         self.call_service("script/lights_set_tv")  
       
     elif old == "playing" and new == "paused":
-      if self.is_controling_lights(self.old_app, current_app):
+      if self.is_app_controling_lights(current_app):
         #CALL SCRIPT
         self.log("TV paused : Lights partially un-dimmed")
         self.call_service("script/lights_set_tv_paused")        
  
     elif old in ["playing" , "paused"] and new in ["standby" , "off" , "unavailable", "unknown", "idle"]:
-      if self.is_controling_lights(self.old_app, current_app):
+      if self.is_app_controling_lights(self.old_app) or self.is_app_controling_lights(current_app):
         #CALL SCRIPT
         self.log("TV stopped : Lights fully un-dimmed")
         self.call_service("script/lights_set_livingroom_kitchen_regular")
@@ -64,17 +64,37 @@ class watch_tv(hass.Hass):
   Helper method:
   Does : 
   . nothing
-  Returns : true if an app is controlling the lightsm false otherwise
-
+  Returns : true if an app is controlling the light, false otherwise
   """
-  def is_controling_lights(self, old_tv_app, current_tv_app):
-    supported_app_ids = [
-      "org.xbmc.kodi", 
+  def is_app_controling_lights(self, tv_app):
+    supported_app_ids = [ 
       "com.netflix.ninja",
       "com.amazon.amazonvideo.livingroom",
       "com.plexapp.android"]
-    if (old_tv_app in supported_app_ids or current_tv_app in supported_app_ids) and self.get_state("sun.sun") == "below_horizon":
+    if tv_app in supported_app_ids and self.get_state("sun.sun") == "below_horizon":
       return True
     else:
       return False
+
+  """
+  Helper method:
+  Does : 
+  . nothing
+  Returns : A human readable format of the app_id if known (the app_id otherwise)
+  """
+  def translate_app(self, tv_app):
+    translations = {
+      "com.netflix.ninja":"Netflix",
+      "com.amazon.amazonvideo.livingroom":"Amazon Prime",
+      "com.plexapp.android":"Plex",
+      "tv.molotov.app":"Molotov",
+      "com.google.android.tvlauncher":"Home",
+      "com.spotify.tv.android":"Spotify",
+      "com.google.android.youtube.tv":"YouTube",
+      "com.google.android.apps.mediashell":"Cast"
+    }
+    if tv_app in translations:
+      return translations[tv_app]
+    else:
+      return tv_app
 
