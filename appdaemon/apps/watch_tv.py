2ac21b35418e0c75b67172488fe7465b66eb8478
jlpouffier <jean.loic.pouffier@gmail.com>
2022-07-24 19:11:41 +0200

    Complete re-write of "Watch TV"

--- appdaemon/apps/watch_tv.py
+++ appdaemon/apps/watch_tv.py
@@ -1,62 +1,78 @@
 import hassapi as hass
 
 """
-watch_tv is an app that listen to state changes of Android TV and dim the lights accordingly 
-Functionalities :
-. Dim lights when some apps are playing (only if sun is set)
-. Undim lights partially when some apps are paused (only if sun is set)
-. Undim lights totally when some apps are stopped (only if sun is set)
 
-List of apps driving Lights:
-. Netflix
-. Plex
-. Amazon Prime
 
 """
 class watch_tv(hass.Hass):
   def initialize(self):
-    # TV CALLBACK
-    self.listen_state(self.callback_philips_android_tv, "media_player.philips_android_tv")
+    # The TV automations will only run if the sun is down.  
+    self.listen_state(self.callback_initialize_automations, "sun.sun", new = "below_horizon", immediate = True)
+    self.listen_state(self.callback_stop_automations, "sun.sun", new = "above_horizon", immediate = True)
 
-    # PS5 Callback
-    self.listen_state(self.callback_ps5, "binary_sensor.is_ps5_used")
+    # handles to register / unregister TV state change callbacks
+    self.state_handles = []
 
-    self.log("Watch TV Automation initialized")
     # Variable to store the old app to detect app changes. Initialized to the Android Home page
     self.old_app = "com.google.android.tvlauncher"
 
+    self.log("Watch TV Automations initialized")
+ 
   """
-  Callback trigerred every time Android TV state change
-  Goals
-  . Dim lights when some apps are playing
-  . Undim lights partially when some apps are paused
-  . Undim lights totally when some apps are stopped 
+
+
   """
-  def callback_philips_android_tv(self, entity, attribute, old, new, kwargs):
-    # Get attrobutes of the TV and fetch the current app
-    attr = self.get_state(entity, attribute = "all")
-    if 'attributes' in attr and 'app_id' in attr["attributes"]:
-      current_app = attr["attributes"]["app_id"] 
+  def callback_initialize_automations(self, entity, attribute, old, new, kwargs):
+    # Register TV state change callbacks 
+    callback_tv_state_change_handle = self.listen_state(self.callback_tv_state_change, "media_player.philips_android_tv", immediate = True)
+    callback_ps5_state_change_handle = self.listen_state(self.callback_ps5_state_change, "binary_sensor.is_ps5_used", immediate = True)
+
+    # Store handles to deregister later.
+    self.state_handles.append(callback_tv_state_change_handle)
+    self.state_handles.append(callback_ps5_state_change_handle)
+
+    self.log("Watch TV Automations starting now ...")
+
+  """
+
+
+  """
+  def callback_stop_automations(self, entity, attribute, old, new, kwargs):
+    # Deregister TV state change callbacks
+    for handle in self.state_handles:
+      self.cancel_listen_state(handle)
+
+    self.log("Watch TV Automations stopping for now ...")
+
+  """
+
+
+  """
+  def callback_tv_state_change(self, entity, attribute, old, new, kwargs):
+    # Get attributes of the TV and fetch the current app
+    tv_attributes = self.get_state(entity, attribute = "all")
+    if 'attributes' in tv_attributes and 'app_id' in tv_attributes["attributes"]:
+      current_app = tv_attributes["attributes"]["app_id"] 
     else:
       current_app = "com.google.android.tvlauncher"
     
     #LOG
-    self.log("State changed: " + old + " @ " + self.translate_app(self.old_app) + " > " + new + " @ " + self.translate_app(current_app))
+    self.log("State changed: " + str(old) + " @ " + self.translate_app(self.old_app) + " > " + str(new) + " @ " + self.translate_app(current_app))
     
-    if old in ["off", "standby", "unavailable" , "paused", "unknown", "idle"] and new == "playing":
+    if old in ["off", "standby", "unavailable" , "paused", "unknown", "idle", None] and new == "playing":
       if self.is_app_controling_lights(current_app):
         #CALL SCRIPT
         self.log("TV playing : Lights dimmed")
         self.call_service("script/lights_set_tv")  
       
-    elif old == "playing" and new == "paused":
+    elif old in ["playing" , None] and new == "paused":
       if self.is_app_controling_lights(current_app):
         #CALL SCRIPT
         self.log("TV paused : Lights partially un-dimmed")
         self.call_service("script/lights_set_tv_paused")        
  
     elif old in ["playing" , "paused"] and new in ["standby" , "off" , "unavailable", "unknown", "idle"]:
-      if self.is_app_controling_lights(self.old_app) or self.is_app_controling_lights(current_app):
+      if (self.is_app_controling_lights(self.old_app) or self.is_app_controling_lights(current_app)):
         #CALL SCRIPT
         self.log("TV stopped : Lights fully un-dimmed")
         self.call_service("script/lights_set_livingroom_kitchen_regular")
@@ -64,19 +80,17 @@ class watch_tv(hass.Hass):
     self.old_app = current_app
 
   """
-  Callback trigerred every time is_ps5_used changes
-  Goals
-  . Dim lights when ps5 used
-  . Undim lights totally when when ps5 stopped
+
+
   """
-  def callback_ps5(self, entity, attribute, old, new, kwargs):
-    if new == 'on' and self.get_state("sun.sun") == "below_horizon":
+  def callback_ps5_state_change(self, entity, attribute, old, new, kwargs):
+    if old in ["off" , None] and new == 'on':
         self.log("PS5 is use : Lights dimmed")
         self.call_service("script/lights_set_tv") 
-    elif new == 'off' and self.get_state("sun.sun") == "below_horizon":
+    elif old == "on" and new == 'off':
         self.log("PS5 stopped : Lights fully un-dimmed")
         self.call_service("script/lights_set_livingroom_kitchen_regular")
-    
+
   """
   Helper method:
   Does : 
@@ -88,7 +102,7 @@ class watch_tv(hass.Hass):
       "com.netflix.ninja",
       "com.amazon.amazonvideo.livingroom",
       "com.plexapp.android"]
-    if tv_app in supported_app_ids and self.get_state("sun.sun") == "below_horizon":
+    if tv_app in supported_app_ids:
       return True
     else:
       return False
@@ -109,7 +123,9 @@ class watch_tv(hass.Hass):
       "com.spotify.tv.android":"Spotify",
       "com.google.android.youtube.tv":"YouTube",
       "com.google.android.apps.mediashell":"Cast",
-      "com.aspiro.tidal":"Tidal"
+      "com.aspiro.tidal":"Tidal",
+      "org.droidtv.playtv": "HDMI Output",
+      "org.droidtv.channels": "Source Selector"
     }
     if tv_app in translations:
       return translations[tv_app]
