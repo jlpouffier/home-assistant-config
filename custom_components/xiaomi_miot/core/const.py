47745247642d335e5f61adda77c6ad2942430b22
jlpouffier <jean.loic.pouffier@gmail.com>
2022-09-06 17:29:23 +0200

    Revert "Delete custom_components directory"
    This reverts commit a45d1f17c153bd73d01f388c7ddd4ac678ea6cef.

---
+++ custom_components/xiaomi_miot/core/const.py
@@ -0,0 +1,88 @@
+from enum import Enum
+
+from .device_customizes import DEVICE_CUSTOMIZES  # noqa
+from .miot_local_devices import MIOT_LOCAL_MODELS  # noqa
+from .translation_languages import TRANSLATION_LANGUAGES  # noqa
+
+DOMAIN = 'xiaomi_miot'
+DEFAULT_NAME = 'Xiaomi Miot'
+
+CONF_MODEL = 'model'
+CONF_XIAOMI_CLOUD = 'xiaomi_cloud'
+CONF_SERVER_COUNTRY = 'server_country'
+CONF_CONN_MODE = 'conn_mode'
+CONF_CONFIG_VERSION = 'config_version'
+
+DEFAULT_CONN_MODE = 'cloud'
+
+SUPPORTED_DOMAINS = [
+    'sensor',
+    'binary_sensor',
+    'switch',
+    'light',
+    'fan',
+    'climate',
+    'cover',
+    'humidifier',
+    'media_player',
+    'camera',
+    'vacuum',
+    'water_heater',
+    'device_tracker',
+    'remote',
+    'alarm_control_panel',
+]
+
+CLOUD_SERVERS = {
+    'cn': '中国大陆',
+    'tw': '中國台灣',
+    'de': 'Europe',
+    'i2': 'India',
+    'ru': 'Russia',
+    'sg': 'Singapore',
+    'us': 'United States',
+}
+
+try:
+    # hass 2020.12.2
+    from homeassistant.components.number import DOMAIN as DOMAIN_NUMBER
+    SUPPORTED_DOMAINS.append(DOMAIN_NUMBER)
+except (ModuleNotFoundError, ImportError):
+    DOMAIN_NUMBER = None
+
+try:
+    # hass 2021.6
+    from homeassistant.components.sensor import STATE_CLASS_MEASUREMENT
+except (ModuleNotFoundError, ImportError):
+    STATE_CLASS_MEASUREMENT = None
+
+try:
+    # hass 2021.7
+    from homeassistant.components.select import DOMAIN as DOMAIN_SELECT
+    SUPPORTED_DOMAINS.append(DOMAIN_SELECT)
+except (ModuleNotFoundError, ImportError):
+    DOMAIN_SELECT = None
+
+try:
+    # hass 2021.9
+    from homeassistant.components.sensor import STATE_CLASS_TOTAL_INCREASING
+except (ModuleNotFoundError, ImportError):
+    STATE_CLASS_TOTAL_INCREASING = None
+
+try:
+    # hass 2021.12
+    from homeassistant.components.button import DOMAIN as DOMAIN_BUTTON
+    SUPPORTED_DOMAINS.append(DOMAIN_BUTTON)
+except (ModuleNotFoundError, ImportError):
+    DOMAIN_BUTTON = None
+
+try:
+    # hass 2022.12
+    from homeassistant.helpers.entity import EntityCategory
+    ENTITY_CATEGORY_VIA_ENUM = True
+except (ModuleNotFoundError, ImportError):
+    class EntityCategory(Enum):
+        CONFIG = 'config'
+        DIAGNOSTIC = 'diagnostic'
+        SYSTEM = 'system'
+    ENTITY_CATEGORY_VIA_ENUM = False
---
+++ custom_components/xiaomi_miot/core/utils.py
@@ -0,0 +1,121 @@
+import os
+import re
+import json
+import locale
+import tzlocal
+from homeassistant.core import HomeAssistant
+from homeassistant.util.dt import DEFAULT_TIME_ZONE, get_time_zone
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+
+def get_manifest(field=None, default=None):
+    manifest = {}
+    with open(f'{os.path.dirname(__file__)}/../manifest.json') as fil:
+        manifest = json.load(fil) or {}
+    return manifest.get(field, default) if field else manifest
+
+
+def local_zone(hass=None):
+    try:
+        if isinstance(hass, HomeAssistant):
+            return get_time_zone(hass.config.time_zone)
+        return tzlocal.get_localzone()
+    except KeyError:
+        pass
+    return DEFAULT_TIME_ZONE
+
+
+def in_china(hass=None):
+    if isinstance(hass, HomeAssistant):
+        if hass.config.time_zone in ['Asia/Shanghai', 'Asia/Hong_Kong']:
+            return True
+    try:
+        return f'{locale.getdefaultlocale()[0]}'[:3] == 'zh_'
+    except (KeyError, Exception):
+        pass
+    return False
+
+
+def wildcard_models(model):
+    if not model:
+        return []
+    if ':' in model:
+        return [model]
+    wil = re.sub(r'\.[^.]+$', '.*', model)
+    return [
+        model,
+        wil,
+        re.sub(r'^[^.]+\.', '*.', wil),
+    ]
+
+
+def is_offline_exception(exc):
+    err = f'{exc}'
+    ret = 'Unable to discover the device' in err
+    if not ret:
+        ret = 'OSError: [Errno 64] Host is down' in err
+    if not ret:
+        ret = 'OSError: [Errno 65] No route to host' in err
+    return ret
+
+
+async def async_analytics_track_event(hass: HomeAssistant, event, action, label, value=0, **kwargs):
+    pms = {
+        'model': label,
+        'event': event,
+        'action': action,
+        'label': label,
+        'value': value,
+        'locale': locale.getdefaultlocale()[0],
+        'tz': hass.config.time_zone,
+        'ver': get_manifest('version', ''),
+        **kwargs,
+    }
+    url = 'https://hacc.miot-spec.com/api/track'
+    try:
+        session = async_get_clientsession(hass)
+        return await session.post(url, data=pms, timeout=3)
+    except (Exception, ValueError):
+        return False
+
+
+class RC4:
+    _idx = 0
+    _jdx = 0
+    _ksa: list
+
+    def __init__(self, pwd):
+        self.init_key(pwd)
+
+    def init_key(self, pwd):
+        cnt = len(pwd)
+        ksa = list(range(256))
+        j = 0
+        for i in range(256):
+            j = (j + ksa[i] + pwd[i % cnt]) & 255
+            ksa[i], ksa[j] = ksa[j], ksa[i]
+        self._ksa = ksa
+        self._idx = 0
+        self._jdx = 0
+        return self
+
+    def crypt(self, data):
+        if isinstance(data, str):
+            data = data.encode()
+        ksa = self._ksa
+        i = self._idx
+        j = self._jdx
+        out = []
+        for byt in data:
+            i = (i + 1) & 255
+            j = (j + ksa[i]) & 255
+            ksa[i], ksa[j] = ksa[j], ksa[i]
+            out.append(byt ^ ksa[(ksa[i] + ksa[j]) & 255])
+        self._idx = i
+        self._jdx = j
+        self._ksa = ksa
+        return bytearray(out)
+
+    def init1024(self):
+        self.crypt(bytes(1024))
+        return self
