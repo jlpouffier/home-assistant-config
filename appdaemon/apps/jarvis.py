f3b8e081fe52e20c5374d02223c0f364fc0cc32b
jlpouffier <jean.loic.pouffier@gmail.com>
2022-10-10 11:00:13 +0200

    Starting to experiment with local voice control

---
+++ appdaemon/apps/jarvis.py
@@ -0,0 +1,25 @@
+import hassapi as hass
+
+class jarvis(hass.Hass):
+    def initialize(self):
+        self.listen_event(self.callback_act_on_switch_entity, "rhasspy_act_on_switch_entity")
+        self.listen_event(self.callback_act_on_light_entity, "rhasspy_act_on_light_entity")
+        self.listen_event(self.callback_act_on_cover_entity, "rhasspy_act_on_cover_entity")
+        self.listen_event(self.callback_act_on_cover_position_entity, "rhasspy_act_on_cover_position_entity")
+
+        self.log("Jarvis initialized")
+    
+    def callback_act_on_switch_entity(self, event_name, data, kwargs):
+        self.call_service(data["action"], entity_id = data["entity"])
+    
+    def callback_act_on_light_entity(self, event_name, data, kwargs):
+        if data["action"] == "light/turn_on":
+            self.call_service(data["action"], entity_id = data["entity"], brightness_pct = 100)
+        else:
+            self.call_service(data["action"], entity_id = data["entity"])
+
+    def callback_act_on_cover_entity(self, event_name, data, kwargs):
+        self.call_service(data["action"], entity_id = "cover.living_room_cover")
+    
+    def callback_act_on_cover_position_entity(self, event_name, data, kwargs):
+        self.call_service("cover/set_cover_position", entity_id = "cover.living_room_cover", position = data["position"])
