735e88443c7be5dbbad3d1228d333e602d88e386
jlpouffier <jean.loic.pouffier@gmail.com>
2022-11-22 09:29:56 +0100

    Heat_home v2

--- appdaemon/apps/heat_home.py
+++ appdaemon/apps/heat_home.py
@@ -9,28 +9,48 @@ Functionalities :
 """
 class heat_home(hass.Hass): 
     def initialize(self):
-        self.listen_state(self.callback_doors_or_windows_open, "binary_sensor.all_doors", new = "on", duration = 60)
-        self.listen_state(self.callback_doors_or_windows_open, "binary_sensor.all_windows", new = "on", duration = 60)
-        self.listen_state(self.callback_doors_or_windows_closed, "binary_sensor.all_doors", new = "off")
-        self.listen_state(self.callback_doors_or_windows_closed, "binary_sensor.all_windows", new = "off")
+        self.listen_state(self.callback_openings_open, "binary_sensor.all_openings", new = "on", duration = 60)
+        self.listen_state(self.callback_openings_closed, "binary_sensor.all_openings", new = "off")
+        self.listen_state(self.callback_home_empty, "binary_sensor.home_occupied", new= "off")
+        self.listen_state(self.callback_home_occupied , "binary_sensor.home_occupied" , new = "on")
+
         self.log("Heat Home automations initialized")
 
     """
     Callback triggered when window / door opened for too long.
     Goals :
-    . Turn off thermostat if heating in progress
+    . Turn off thermostat
     """
-    def callback_doors_or_windows_open(self, entity, attribute, old, new, kwargs):
-        if self.get_state("climate.netatmo", attribute = "hvac_action") == "heating":
-            self.log("Window or door opened, and heating in progress ... stopping thermostat temporarly.")
+    def callback_openings_open(self, entity, attribute, old, new, kwargs):
+        if self.get_state("climate.netatmo") != 'off':
+            self.log("Window or door opened, and thermostat not stopped ... stopping thermostat temporarly.")
             self.call_service("climate/turn_off", entity_id = "climate.netatmo")
 
     """
     Callback triggered when window / door closed
     Goals :
-    . Turn on thermostat if stopped before
+    . Turn on thermostat 
     """
-    def callback_doors_or_windows_closed(self, entity, attribute, old, new, kwargs):
+    def callback_openings_closed(self, entity, attribute, old, new, kwargs):
         if self.get_state("climate.netatmo") == 'off':
             self.log("Window or door closed, and thermostat stopped ... restating thermostat.")
             self.call_service("climate/turn_on", entity_id = "climate.netatmo")
+
+    """
+    Callback triggered when home become empty
+    Goals :
+    . set thermostat mode to Away
+    """
+    def callback_home_empty(self, entity, attribute, old, new, kwargs):
+        self.log("Home empty: Setting the thermostat mode to Away")
+        self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Absent 16")
+
+    """
+    Callback triggered when home become empty
+    Goals :
+    . set thermostat mode to Present
+    """
+    def callback_home_occupied(self, entity, attribute, old, new, kwargs):
+        self.log("Home empty: Setting the thermostat mode to Present")
+        self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Present")
+
