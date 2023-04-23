2e997d2c5500082d341205377b0f51d3daf5de54
jlpouffier <jean.loic.pouffier@gmail.com>
2023-04-23 20:58:42 +0200

    Heat_home is now local 🎉

--- appdaemon/apps/heat_home.py
+++ appdaemon/apps/heat_home.py
@@ -1,64 +1,96 @@
 import hassapi as hass
+import datetime
 
 """
 heat_home is an app responsible of driving the home thermostat to reduce energy loss
 
 Functionalities :
-    Turn off thermostat if windows / doors opened for too long
+    Frost guard if windows / doors opened for too long
     Turn on again thermostat if stopped by windows / doors opened for too long
 """
 class heat_home(hass.Hass): 
     def initialize(self):
-        self.listen_state(self.callback_openings_open, "binary_sensor.is_home_open_since_more_than_one_minute", new = "on")
-        self.listen_state(self.callback_openings_closed, "binary_sensor.is_home_open_since_more_than_one_minute", new = "off")
-        self.listen_state(self.callback_home_empty, "binary_sensor.home_occupied", new= "off")
-        self.listen_state(self.callback_home_occupied , "binary_sensor.home_occupied" , new = "on")
+        self.listen_state(self.callback_conditions_changed, ["binary_sensor.is_home_open_since_more_than_one_minute", "binary_sensor.home_occupied"], immediate = True)
+        self.listen_state(self.callback_heating_schedule_selector_updated, "input_select.heating_schedule_selector", immediate = True)
+        time = datetime.time(0, 0, 0)
+        self.run_minutely(self.callback_check_temperature_coherency, time)
 
     """
-    Callback triggered when window / door opened for too long.
+    Callback triggered when    
+        window / door opened or for too long.
+        home becomes occupied or not
     Goals :
-        Turn off thermostat
+        Change Heating Schedule Selector
     """
-    def callback_openings_open(self, entity, attribute, old, new, kwargs):
-        self.log("Window or door opened ... Setting the thermostat mode to Frost Guard.")
-        self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Protection Gel 5")
-
-    """
-    Callback triggered when window / door closed
-    Goals :
-        Turn on thermostat 
-    """
-    def callback_openings_closed(self, entity, attribute, old, new, kwargs):
-        if self.entities.binary_sensor.home_occupied.state == "on":
-            self.log("Home occupied and closed: Setting the thermostat mode to Present")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Present")
-        else:
-            self.log("Home empty and closed: Setting the thermostat mode to Away")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Absent 16")
-
-    """
-    Callback triggered when home become empty
-    Goals :
-        Set thermostat mode to Away
-    """
-    def callback_home_empty(self, entity, attribute, old, new, kwargs):
+    def callback_conditions_changed(self, entity, attribute, old, new, kwargs):
         if self.entities.binary_sensor.is_home_open_since_more_than_one_minute.state == "on":
-            self.log("Home empty and open: Setting the thermostat mode to Frost Guard")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Protection Gel 5")
-        else:
-            self.log("Home empty and closed: Setting the thermostat mode to Away")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Absent 16")
+            self.log("Window or door opened ... Setting the thermostat mode to Frost Guard.")
+            self.call_service("input_select/select_option", entity_id = "input_select.heating_schedule_selector", option = "Frost Guard")
+        elif self.entities.binary_sensor.home_occupied.state == "on":
+            self.log("Home occupied: Setting the thermostat mode to Present")
+            self.call_service("input_select/select_option", entity_id = "input_select.heating_schedule_selector", option = "Présent")
+        elif self.entities.binary_sensor.home_occupied.state == "off":
+            self.log("Home empty: Setting the thermostat mode to Away")
+            self.call_service("input_select/select_option", entity_id = "input_select.heating_schedule_selector", option = "Absent")
+        
 
     """
-    Callback triggered when home become empty
+    Callback triggered Heating Schedule Selector changes
     Goals :
-        Set thermostat mode to Present
+        compute the target temperature
+        compare it the the current target temperature
+        change it if needed
     """
-    def callback_home_occupied(self, entity, attribute, old, new, kwargs):
-        if self.entities.binary_sensor.is_home_open_since_more_than_one_minute.state == "on":
-            self.log("Home occupied and open: Setting the thermostat mode to Frost Guard")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Protection Gel 5")
-        else:
-            self.log("Home occupied and closed: Setting the thermostat mode to Present")
-            self.call_service("select/select_option", entity_id = "select.planning_netatmo", option = "Present")
+    def callback_heating_schedule_selector_updated(self, entity, attribute, old, new, kwargs):
+        target_temperature = self.set_target_temperature()
+        current_temperature = int(self.entities.climate.netatmo.attributes.temperature)
 
+        if target_temperature != current_temperature:
+            self.log("Mode changed ... Changing temperature")
+            self.call_service("climate/set_temperature", entity_id = "climate.netatmo", temperature = target_temperature)
+    
+    """
+    Callback triggered every minute
+    Goals :
+        compute the target temperature
+        compare it the the current target temperature
+        change it if needed
+    """
+    def callback_check_temperature_coherency(self, kwargs):
+        target_temperature = self.set_target_temperature()
+        current_temperature = int(self.entities.climate.netatmo.attributes.temperature)
+
+        if target_temperature != current_temperature:
+            self.log("Temperature not coherent ... Changing temperature")
+            self.call_service("climate/set_temperature", entity_id = "climate.netatmo", temperature = target_temperature)
+
+    """
+    Helper method:
+    Does : 
+        nothing
+    Returns : 
+        The target temperature right now based on the Heating Schedule Selector and the calendar entities.
+    """
+    def set_target_temperature(self):
+        if self.entities.input_select.heating_schedule_selector.state == "Présent":
+            target_calendar = "calendar.heating_schedule_present"
+            default_temperature = 17
+        elif self.entities.input_select.heating_schedule_selector.state == "Absent":
+            target_calendar = "calendar.heating_schedule_absent"
+            default_temperature = 16
+        elif self.entities.input_select.heating_schedule_selector.state == "Frost Guard":
+            target_calendar = "calendar.heating_schedule_frost_guard"
+            default_temperature = 7
+
+        if self.get_state(target_calendar) == "on":
+            try:
+                target_temperature = int(self.get_state(target_calendar, attribute = "message"))
+            except:
+                self.log("WARNING ! CALENDAR ENTRY INVALD, DEFAULTING")
+                target_temperature = default_temperature
+        else:
+            self.log("WARNING ! CALENDAR IS OFF... IT SHOULD NOT HAPPEN, DEFAULTING")
+            target_temperature = default_temperature
+        
+        return target_temperature
+        
