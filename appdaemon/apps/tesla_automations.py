d361e5a1990b4679997c5e814be9c7b55a367435
jlpouffier <jean.loic.pouffier@gmail.com>
2023-05-13 00:52:23 +0200

    New notification (Tesla charge and off peak hour)

--- appdaemon/apps/tesla_automations.py
+++ appdaemon/apps/tesla_automations.py
@@ -1,5 +1,6 @@
 import hassapi as hass
 import json
+import datetime
 
 """
 This app is responsible of all the automation related to my tesla.
@@ -7,7 +8,7 @@ Functionality :
 . Pack location update (Latitude + Longitude) to brodcast the update to MQTT so that it is integrated via mqtt.device_tracker.
 
 Notification :
-. None
+. End of charge after off-peak hour.
 
 """
 
@@ -24,6 +25,7 @@ class tesla_automations(hass.Hass):
         self.update_time_difference = 0
 
         self.listen_state(self.callback_location_updated, ['sensor.tesla_latitude','sensor.tesla_longitude'])
+        self.listen_state(self.callback_tesla_charging_session_started, "sensor.tesla_state", new = "charging", duration = 60)
 
     """
     Callback triggered when either the latitude or the longitude of my tesla is updated
@@ -46,3 +48,30 @@ class tesla_automations(hass.Hass):
             self.call_service('mqtt/publish', qos = '0', retain = True, topic ='tesla/location', payload =  json.dumps(self.location))
             self.update_ts['latitude'] = 0
             self.update_ts['longitude'] = 0
+
+    """
+    Callback triggered when the tesla charges
+    Goals :
+        Check if tesla home
+        Check if we are currently in off peak hour
+        check if the charge end time will be after the end of the off-peak hour.
+            Notify if all the above is true
+    """
+    def callback_tesla_charging_session_started(self, entity, attribute, old, new, kwargs):
+        if self.entities.device_tracker.tesla_device_tracker.state == "home" and self.entities.schedule.heures_creuses.state == "on":
+            
+            planned_charge_end_time = self.get_now() + datetime.timedelta(hours = float(self.entities.sensor.tesla_time_to_full_charge.state))
+            off_peak_end_time = self.convert_utc(self.entities.schedule.heures_creuses.attributes.next_event)
+            
+            # TODO: Calculate new target to include it in the notification
+
+            if planned_charge_end_time > off_peak_end_time:
+                self.log("The end of the charge will be after off peak hour... Notifying")
+                self.fire_event("NOTIFIER",
+                    action = "send_to_jl",
+                    title = "️🚗 Tesla", 
+                    message = "La recharge est prevue de finir après la fin des heures creuse (" + str(planned_charge_end_time.strftime("%H:%M")) + "). Pense à reduire la cibe de recharge",
+                    click_url="/lovelace/tesla",
+                    icon =  "mdi:car",
+                    color = "deep-orange",
+                    tag = "tesla_end_charge_after_off_peak_hours")
