0d00e35f6d4b3bbb8153a9afe89079cad15a2b15
jlpouffier <jean.loic.pouffier@gmail.com>
2023-06-08 17:24:53 +0200

    Office Automation v3

--- appdaemon/apps/office_automations.py
+++ appdaemon/apps/office_automations.py
@@ -12,11 +12,8 @@ class office_automations(hass.Hass):
     def initialize(self):
         self.listen_state(self.callback_webcam_in_use, "binary_sensor.jean_loics_laptop_camera_in_use", new = "on")
         self.listen_state(self.callback_webcam_not_in_use, "binary_sensor.jean_loics_laptop_camera_in_use", new = "off")
-        self.listen_state(self.callback_presence_detected, "binary_sensor.office_presence_sensor_occupancy", new = "on")
-        self.listen_state(self.callback_presence_not_detected, "binary_sensor.office_presence_sensor_occupancy", new = "off")
         self.listen_state(self.callback_vr_in_progress, "binary_sensor.quest_in_use", new = "on")
         self.listen_state(self.callback_vr_not_in_progress, "binary_sensor.quest_in_use", new = "off")
-        
         
     def callback_webcam_in_use(self, entity, attribute, old, new, kwargs):
         is_jl_present = True if self.entities.person.jenova70.state == "home" else False
@@ -25,38 +22,34 @@ class office_automations(hass.Hass):
 
         if is_jl_present and is_jl_laptop_present and is_office_occupied:
             self.log("Webcam in use ... turning on face lights")
-            self.call_service("light/turn_on", entity_id = "light.hue_play_bars", brightness_pct = 60)
+            # Saving current light state in the office
+            self.call_service("scene/create", scene_id = "office_previous_light_state", snapshot_entities = ["light.bureau"])
+            # Turning on lights
+            self.call_service("light/turn_on", entity_id = "light.bureau", brightness_pct = 100, color_temp_kelvin = 3521)
+            self.call_service("light/turn_on", entity_id = "light.hue_play_bars", brightness_pct = 60, color_temp_kelvin = 3521)
     
     def callback_webcam_not_in_use(self, entity, attribute, old, new, kwargs):
         if self.entities.light.hue_play_bars.state == "on":
             self.log("Webcam not in use ... turning off face lights")
+            # Turning off lights
             self.call_service("light/turn_off", entity_id = "light.hue_play_bars")
+            # Restoring previous light state in the office
+            try:
+                self.call_service("scene/turn_on", entity_id = "scene.office_previous_light_state")
+            except:
+                self.call_service("scene/turn_on", entity_id = "scene.bureau_bureau_100")
     
-    def callback_presence_detected(self, entity, attribute, old, new, kwargs):
-        is_workday = True if self.entities.binary_sensor.workday_today.state == "on" else False
-        is_working_hours = True if self.entities.schedule.working_hours.state == "on" else False
-        is_jl_present = True if self.entities.person.jenova70.state == "home" else False
-
-        if is_workday and is_working_hours and is_jl_present:
-            self.log("Presence detected, during a working day and working hour... Turning on lights")
-            self.call_service("light/turn_on", entity_id = "light.bureau", brightness_pct = 100)
-
-    def callback_presence_not_detected(self, entity, attribute, old, new, kwargs):
-        is_workday = True if self.entities.binary_sensor.workday_today.state == "on" else False
-        is_working_hours = True if self.entities.schedule.working_hours.state == "on" else False
-        is_jl_present = True if self.entities.person.jenova70.state == "home" else False
-
-        if is_workday and is_working_hours and is_jl_present:
-            self.log("Presence not detected, during a working day and working hour... Turning off lights")
-            self.call_service("light/turn_off", entity_id = "light.bureau")
-        
     def callback_vr_in_progress(self, entity, attribute, old, new, kwargs):
         is_jl_present = True if self.entities.person.jenova70.state == "home" else False
         is_office_occupied = True if self.entities.binary_sensor.office_presence_sensor_occupancy.state == "on" else False
 
         if is_jl_present and is_office_occupied:
             self.log("VR in progress... TODO")
-            self.call_service("light/turn_on", entity_id = "light.hue_play_bars", brightness_pct = 100)
+            # Saving current light state in the office
+            self.call_service("scene/create", scene_id = "office_previous_light_state", snapshot_entities = ["light.bureau"])
+            # Turning on lights
+            self.call_service("light/turn_on", entity_id = "light.bureau", brightness_pct = 100, color_temp_kelvin = 3521)
+            self.call_service("light/turn_on", entity_id = "light.hue_play_bars", brightness_pct = 100, color_temp_kelvin = 3521)
     
     def callback_vr_not_in_progress(self, entity, attribute, old, new, kwargs):
         is_jl_present = True if self.entities.person.jenova70.state == "home" else False
@@ -64,5 +57,11 @@ class office_automations(hass.Hass):
 
         if is_jl_present and is_office_occupied:
             self.log("VR stopped... TODO")
+            # Turning off lights
             self.call_service("light/turn_off", entity_id = "light.hue_play_bars")
+            # Restoring previous light state in the office
+            try:
+                self.call_service("scene/turn_on", entity_id = "scene.office_previous_light_state")
+            except:
+                self.call_service("scene/turn_on", entity_id = "scene.bureau_bureau_100")
 
